12902b045c8534be9c575170ae1cd475f2e537e4
Zach Lualhati <zachtlualhati@gmail.com>
2022-06-24 20:11:40 +1000

    asd

---
+++ js/index.js
@@ -0,0 +1,46 @@
+var map = L.map('map').setView([-24, 133], 5);
+L.tileLayer('https://api.mapbox.com/styles/v1/zachtl/cl49vflg1000115ljhl7ubnta/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiemFjaHRsIiwiYSI6ImNsMXczNnJqMjFybjAzYnM2MzBpY2NtdXoifQ.403YTFq9aBWL-BYxVDO6NA', {
+    attribution: '© <a href="https://www.mapbox.com/map-feedback/">Mapbox</a> © <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    dragging: false,
+    zoomControl: false
+}).addTo(map);
+
+map.touchZoom.disable();
+map.doubleClickZoom.disable();
+map.scrollWheelZoom.disable();
+map.boxZoom.disable();
+map.keyboard.disable();
+$(".leaflet-control-zoom").css("visibility", "hidden");
+
+var settings = {
+    // url: "https://cors-anywhere.herokuapp.com/https://www.bnefoodtrucks.com.au/api/1/private-bookings",
+    url: "https://www.bnefoodtrucks.com.au/api/1/private-bookings",
+    method: "GET",
+    headers: {
+        "Content-Type": "application/json"
+    }
+
+}
+
+function fly() {
+    map.flyTo([-27.270125, 153.021072], 9, {
+        animate: true,
+        duration: 5,
+    });
+
+    $.ajax(settings).done(function (results) {
+        console.log(
+            ((results[0].Geolocation).slice(31, -2)).split(/[, ]+/)[0]
+        )
+        console.log(results);
+
+        for (var i = 0; i < results.length; i++) {
+            var marker = L.marker([(((results[i].Geolocation).slice(31, -2)).split(/[, ]+/)[1]), (((results[i].Geolocation).slice(31, -2)).split(/[, ]+/)[0])]).addTo(map)
+            marker.bindPopup("<h1>" + results[i].Title + "</h1>");
+        }
+    })
+
+    console.log(e)
+}
+
+fly();
--- main.php
+++ main.php
@@ -16,16 +16,10 @@
     <?php include("nav.php") ?>
     <!--Page Content-->
     <main>
-        <div class="px-4 text-center align-items-center index-back text-white">
-            <div id="map"></div>
-            <div class="align-self-center blur" style="text-shadow: 0 0 10px #000000">
-                <h1 class="display-5 fw-bold">Food on Wheels</h1>
-                <div class="col-lg-6 mx-auto">
-                    <p class="lead mb-4">View food trucks in Queensland</p>
-                    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-                        <a type="button" class="btn btn-light btn-lg px-4 gap-3" href="./main">Lets have a look!</a>
-                    </div>
-                </div>
+        <div class="px-3 py-5 img1 text-white">
+            <div class="containe px-3 py-3">
+                <h1 class="display-1">Find whats up</h1>
+                <p class="lead">Look through the active and future events for food trucks</p>
             </div>
         </div>
     </main>
@@ -34,6 +28,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <script src="./js/index.js" type="text/javascript"></script>
+    <script src="./js/main.js" type="text/javascript"></script>
 </body>
+
 </html>
